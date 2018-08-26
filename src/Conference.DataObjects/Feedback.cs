1dd108a01b9851657380a028ba405d2a359453ee
L0ndra <illialubenets@yandex.ua>
2018-08-26 19:11:38 +0300

    add func solution

---
+++ src/Conference.DataObjects/Feedback.cs
@@ -0,0 +1,16 @@
+﻿namespace Conference.DataObjects
+{
+    /// <summary>
+    /// Per user feedback
+    /// </summary>
+    public class Feedback : BaseDataObject
+    {
+        public string UserId { get; set; }
+        public string SessionId { get; set; }
+        public int SessionRating { get; set; }
+        public int Prepeared { get; set; }
+        public int Learnnew { get; set; }
+        public int StayedinFocus { get; set; }
+        public int Expertise { get; set; }
+    }
+}
